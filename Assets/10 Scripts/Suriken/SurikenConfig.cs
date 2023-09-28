e1b986683e48598a5b7be254f51f4dcdf75b55f4
ralvac178 <52391112+ralvac178@users.noreply.github.com>
2023-09-28 17:13:56 -0500

    Add Horizontal Suriken

---
+++ Assets/10 Scripts/Suriken/SurikenConfig.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "SurikenData", menuName = "ScriptableObjects/Suriken" +
+    "", order = 1)]
+public class SurikenConfig : ScriptableObject
+{
+    public string type;
+}
--- Assets/10 Scripts/Suriken/SurikenController.cs
+++ Assets/10 Scripts/Suriken/SurikenController.cs
@@ -4,19 +4,33 @@ using UnityEngine;
 
 public class SurikenController : MonoBehaviour
 {
+    [SerializeField] private SurikenConfig config;
     [SerializeField] private float offset;
     private Vector2 initPosition;
     private bool turnDown;
+    private string type;
     [SerializeField] private float force;
     // Start is called before the first frame update
     void Start()
     {
         initPosition = transform.position;
-        //transform.Translate
+        type = config.type;
     }
 
     // Update is called once per frame
     void Update()
+    {
+        if (type.Equals("Vertical"))
+        {
+            MoveVertical();
+        }
+        else
+        {
+            MoveHorizontal();
+        }
+    }
+
+    private void MoveVertical()
     {
         if (!turnDown && (transform.position.y > initPosition.y - offset))
         {
@@ -31,6 +45,26 @@ public class SurikenController : MonoBehaviour
             turnDown = true;
         }
         else if (turnDown && (transform.position.y > initPosition.y))
+        {
+            turnDown = false;
+        }
+    }
+
+    private void MoveHorizontal()
+    {
+        if (!turnDown && (transform.position.x < initPosition.x + offset))
+        {
+            transform.Translate(Vector2.right * force * Time.deltaTime);
+        }
+        else if (turnDown && (transform.position.x > initPosition.x))
+        {
+            transform.Translate(Vector2.left * force * Time.deltaTime);
+        }
+        else if (!turnDown && (transform.position.x > initPosition.x + offset))
+        {
+            turnDown = true;
+        }
+        else if (turnDown && (transform.position.x < initPosition.x))
         {
             turnDown = false;
         }
