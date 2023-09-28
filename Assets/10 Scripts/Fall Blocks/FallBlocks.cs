02b33951d4d3505ae3af16b89d58a12dd8c3e8c5
ralvac178 <52391112+ralvac178@users.noreply.github.com>
2023-09-28 15:10:37 -0500

    Added falling rocks animations and colliders algoritm

---
+++ Assets/10 Scripts/Fall Blocks/FallBlocks.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallBlocks : MonoBehaviour
+{
+    private float timer;
+    [SerializeField] private Animator[] animatorArray;
+
+    private void Start()
+    {
+        animatorArray = GetComponentsInChildren<Animator>();
+    }
+
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            timer += Time.deltaTime;
+            if (timer >= 1.25f)
+            {
+                foreach (var animator in animatorArray)
+                {
+                    animator.SetTrigger("Breaking");
+                }
+                timer = 0;
+            }
+        }     
+    }
+}
